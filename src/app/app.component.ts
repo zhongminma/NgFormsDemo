17b0a978e40010323f0aad0771f5eb2d058b6630
Kevin Ma <ma65536@gmail.com>
2020-03-16 05:06:31 -0400

    Reactive Form login demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-form';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(){
+    this.createForm();
+  }
+  public createForm() {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+  onSubmit() {
+    this.submitted = true;
+    if (this.myForm.invalid) { // stop here if form is invalid
+        return;
+    }
+    console.log(this.myForm.value);
+  }
 }
